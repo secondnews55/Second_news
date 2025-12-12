85ac4427029226c6f38f9e798b141b6bdbd8cce6
secondnews55 <altibbritibbri@gmail.com>
2025-12-12 17:32:09 +0700

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const menuBtn = document.querySelector(".menu-btn");
+    const nav = document.querySelector("nav");
+
+    menuBtn.addEventListener("click", () => {
+        nav.classList.toggle("active");
+    });
+});
